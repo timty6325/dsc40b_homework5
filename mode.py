96341f6980e87a37d505a8b90ef54e1cb4468b53
Timothy <trantimothy88@gmail.com>
2026-05-06 16:14:29 -0700

    finished

--- mode.py
+++ mode.py
@@ -1,17 +1,18 @@
 def mode(numbers):
 	count = {}
 	curr_mode = None
-	curr_freq = None
+	curr_freq = 0
+
 
 	for i in numbers:
 		if i not in count:
 			count[i] = 1
 		else:
 			count[i] += 1
+	
 
 	for key, val in count.items():
-		curr_freq = val
-		curr_mode = key
+		
 		if val > curr_freq:
 			curr_freq = val
 			curr_mode = key
